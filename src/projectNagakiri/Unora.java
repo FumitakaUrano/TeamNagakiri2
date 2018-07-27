e27b012a671cb9b052b08db17a902c6f9b7df077
z00s600113 <z00s600113@ZRFG046919.rfg.rfgad.ricoh.com>
2018-07-27 11:47:01 +0900

    test

--- src/projectNagakiri/Unora.java
+++ src/projectNagakiri/Unora.java
@@ -3,8 +3,13 @@ package projectNagakiri;
 public class Unora {
 
 	public static void main(String[] args) {
-
-
+		fib(1,1,1000);
 	}
 
+	public static void fib(int seed,int next,int threshold){
+		if(seed <=threshold){
+			System.out.print(seed+" ");
+			fib(next,seed+next,threshold);
+		}
+	}
 }
